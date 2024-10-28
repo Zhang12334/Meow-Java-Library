09b6cc1ef930a24151bd4621c772755a2a4d2b0b
Zhang12334 <3143448821@qq.com>
2024-10-29 00:08:39 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-// next.config.js
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['bing.img.run'], // 允许从该域名加载图片
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
