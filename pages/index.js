b3a94fec452043b42a23247f827e24ce4cb8118a
Zhang12334 <3143448821@qq.com>
2024-10-29 00:06:08 +0800

    fix

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ const blurAmount = 5; // 自定义背景模糊程度
 
 export default function Home() {
   return (
-    <div className="container" style={{ backgroundImage: `url(${backgroundImageUrl})`, filter: `blur(${blurAmount}px)` }}>
+    <div className="container">
       <Head>
         <title>Meow Java Library</title>
         <meta name="description" content="A simple Node.js app with a UI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className="background" style={{ backgroundImage: `url(${backgroundImageUrl})`, filter: `blur(${blurAmount}px)` }}></div>
 
       <main className="main">
         <div className="box">
