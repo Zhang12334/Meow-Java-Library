facff97e32075d97a9996e970498f69599fcfd30
Zhang12334 <3143448821@qq.com>
2024-10-29 14:10:07 +0800

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Head from 'next/head';
+import { useEffect, useRef } from 'react';
 
 const backgroundImageUrl = 'https://pan.ofs.moe:65001/d/tuchuang/MeowJavaLibrary/background/th.dmg'; // 自定义背景图片 URL
 const blurAmount = 5; // 自定义背景模糊程度
 
 export default function Home() {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const text = 'Meow Java Library';
+    let index = 0;
+
+    const interval = setInterval(() => {
+      if (index < text.length) {
+        titleRef.current.textContent += text.charAt(index);
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100); // 调整这个值可以改变打字速度
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -16,7 +35,7 @@ export default function Home() {
 
       <main className="main">
         <div className="box">
-          <h1 className="title">Meow Java Library</h1>
+          <h1 className="title" ref={titleRef}></h1>
         </div>
       </main>
     </div>
