0bd1ad5617d588bd52a45e05d4522098d69d8086
Zhang12334 <3143448821@qq.com>
2024-10-29 14:23:24 +0800

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
 import Head from 'next/head';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 const backgroundImageUrl = 'https://bing.img.run/uhd.php'; // 自定义背景图片 URL
+const blurAmount = 5; // 自定义背景模糊程度
 
 export default function Home() {
-  const [bgImageUrl, setBgImageUrl] = useState(null);
   const titleRef = useRef(null);
 
   useEffect(() => {
-    // 获取背景图片 URL
-    fetch(backgroundImageUrl)
-      .then(response => {
-        if (response.ok) {
-          return response.url; // 获取重定向后的 URL
-        } else {
-          throw new Error('Failed to fetch background image');
-        }
-      })
-      .then(url => {
-        setBgImageUrl(url);
-      })
-      .catch(error => {
-        console.error('Error fetching background image:', error);
-      });
-
-    // 打字效果
     const text = 'Meow Java Library';
     let index = 0;
 
@@ -48,9 +31,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {bgImageUrl && (
-        <div className="background" style={{ backgroundImage: `url(${bgImageUrl})` }}></div>
-      )}
+      <div className="background" style={{ backgroundImage: `url(${backgroundImageUrl})`, filter: `blur(${blurAmount}px)` }}></div>
 
       <main className="main">
         <div className="box">
